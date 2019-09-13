29e314e8ad0d87e4cb6e4633f1367e484652966e
Shubhangini7 <khare7shubh@gmail.com>
2019-09-13 15:16:22 +0530

    initial commit after choosing project defination

---
+++ Criminal-Record-Management/src/main/java/com/JFS/Criminal/Record/Management/CriminalRecordManagementApplication.java
@@ -0,0 +1,14 @@
+package com.JFS.Criminal.Record.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriminalRecordManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriminalRecordManagementApplication.class, args);
+		
+	}
+
+}
---
+++ Criminal-Record-Management/src/test/java/com/JFS/Criminal/Record/Management/CriminalRecordManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.JFS.Criminal.Record.Management;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CriminalRecordManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
