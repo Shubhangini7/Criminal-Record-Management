c69fde9e8511fbb50a63e9b88f823ad1b6834cba
Shubhangini7 <khare7shubh@gmail.com>
2019-09-14 09:27:26 +0530

    commit after completing jdbc connection

--- Criminal-Record-Management/src/main/java/com/JFS/Criminal/Record/Management/CriminalRecordManagementApplication.java
+++ Criminal-Record-Management/src/main/java/com/JFS/Criminal/Record/Management/CriminalRecordManagementApplication.java
@@ -1,13 +1,17 @@
 package com.JFS.Criminal.Record.Management;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class CriminalRecordManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CriminalRecordManagementApplication.class, args);
+		System.out.println("prints the connection has established successfully");
 		
 	}
 
