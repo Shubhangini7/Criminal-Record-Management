42f461b317a1ee8d1106f41aed399b5f0a9d77db
Shubhangini7 <khare7shubh@gmail.com>
2019-09-16 15:45:07 +0530

    commit after creating tables

--- Criminal-Record-Management/src/main/java/com/JFS/Criminal/Record/Management/CriminalRecordManagementApplication.java
+++ Criminal-Record-Management/src/main/java/com/JFS/Criminal/Record/Management/CriminalRecordManagementApplication.java
@@ -1,17 +1,13 @@
 package com.JFS.Criminal.Record.Management;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) //connection establish by auto configuration
 public class CriminalRecordManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CriminalRecordManagementApplication.class, args);
-		System.out.println("connection has established successfully");
 		
 	}
 
